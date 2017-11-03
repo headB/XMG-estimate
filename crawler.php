25a0e8dfdc045dd54abc41a993d6c86dcda17c87
kumanxuan <775121173@qq.com>
2017-11-03 16:15:51 +0800

    已经成功在docker里面的scrapy爬虫并且成功将爬虫信息放到数据库了。现在修改一下界面。

--- crawler.php
+++ crawler.php
@@ -2,8 +2,9 @@
 $title= 'crawler';
 include 'head-nav.php';
 include 'view/tableView.php';
+include 'function/function.php';
 
-echo "<h1>千年虫与化骨龙！</h1>";
+echo "<h1>千年蟲與化骨龍，大家好，我系張家輝</h1>";
 
 
 $title = '';
@@ -16,6 +17,22 @@ $content[] = 'y';
 $contentArray[] = $content;
 
 listTable($title,$contentArray);
+
+echo "<p>一下信息，均为测试，没有什么特殊意思！，尽量已经去除重复的！</p>";
+
+$title='';
+$content='';
+$contentArray = '';
+
+
+$title[] = '网站的标题';
+$title[] = '网站的关键字描述';
+
+$db->dataBase = 'crawl';
+
+print_r($db);
+
+$content[] = '';
 
 
 echo "</center></body></html>";
