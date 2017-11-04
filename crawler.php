9b8d503b36584023f9857d5105509843f6d9eb51
kuman-lenovo <775121173@qq.com>
2017-11-04 12:44:43 +0800

    修改了部分的界面。

--- crawler.php
+++ crawler.php
@@ -6,15 +6,32 @@ include 'function/function.php';
 
 echo "<h1>千年蟲與化骨龍，大家好，我系張家輝</h1>";
 
+$db_res->dataBase = 'crawl';
+$db_res->crawl();
 
-$title = '';
+//这里想弄一个变量，里面保存所有机构的id代码。
+$companyInfo = $db_res->query("select * from site");
+
+
+
+$title='';
+$content='';
+$contentArray = '';
 
 $title[] = '机构名称';
 $title[] = '网址';
 
-$content[] = 'x';
-$content[] = 'y';
-$contentArray[] = $content;
+foreach($companyInfo['values'] as $value){
+
+    $content[] = $value['name'];
+    $content[] = $value['url'];
+    $contentArray[] = $content;
+    $content='';
+
+}
+
+
+
 
 listTable($title,$contentArray);
 
@@ -24,18 +41,25 @@ $title='';
 $content='';
 $contentArray = '';
 
-
+$title[] = '归属';
 $title[] = '网站的标题';
 $title[] = '网站的关键字描述';
 
-$db_res->dataBase = 'crawl';
-$db_res->crawl();
+
 //忘记了，因为默认的数据库连接是xingzheng的，得写这条命令才可以执行连接新数据库
 
 
-//这里想弄一个变量，里面保存所有机构的id代码。
 
-$companyInfo = $db_res->query("select * from site");
+
+
+
+$tidInfoMark='';
+
+foreach($companyInfo['values'] as $value){
+    $id = $value['id'];
+    $tidInfoMark[$id] = $value['name'];
+
+}
 
 
 
@@ -43,6 +67,8 @@ $res = $db_res->query("select * from page");
 
 foreach($res['values'] as $values){
 
+    $id = $values['tid'];
+    $content[] = $tidInfoMark[$id];
     $content[] = $values['title'];
     $content[] = $values['keyword'];
     $contentArray[] = $content;
@@ -50,9 +76,9 @@ foreach($res['values'] as $values){
 
 }
 
-
+echo "<div style='width:800px' >";
 listTable($title,$contentArray);
-
+echo "</div>";
 
 
 echo "</center></body></html>";
